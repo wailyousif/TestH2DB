cf98095369cff7fdcc2663f97570709bb4978444
Wail Yousif <wail.yousif@agile-ict.com>
2018-01-12 11:59:09 +0200

    Updated with multithreads

--- src/TestH2.java
+++ src/TestH2.java
@@ -1,9 +1,6 @@
 import org.h2.jdbcx.JdbcConnectionPool;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Created by wailm.yousif on 1/11/18.
@@ -47,19 +44,95 @@ public class TestH2 {
             //conn = DriverManager.getConnection(DB_URL,USER,PASS);
             conn = jdbcConnectionPool.getConnection();
 
+            conn.setAutoCommit(false);
+
             System.out.println("Current Active connections in Connections Pool=" + jdbcConnectionPool.getActiveConnections());
 
             //STEP 3: Execute a query
             System.out.println("Creating table in given database...");
+
             stmt = conn.createStatement();
-            String sql =  "CREATE TABLE   REGISTRATION " +
+            String sql =  "CREATE TABLE TRANS " +
                     "(id INTEGER not NULL, " +
-                    " first VARCHAR(255), " +
-                    " last VARCHAR(255), " +
-                    " age INTEGER, " +
+                    " transtype VARCHAR(255), " +
                     " PRIMARY KEY ( id ))";
+
             stmt.executeUpdate(sql);
-            System.out.println("Created table in given database...");
+            System.out.println("Table created...");
+
+            sql = "Insert into Trans (id, transtype) values (1, 'p2p')";
+            stmt.executeUpdate(sql);
+            System.out.println("First insert done...");
+
+            sql = "Insert into Trans (id, transtype) values (2, 'p2m')";
+            stmt.executeUpdate(sql);
+            System.out.println("Second insert done...");
+
+            conn.commit();
+
+            PreparedStatement preparedStatement = conn.prepareStatement("Select id, transtype from Trans");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next())
+            {
+                System.out.println(resultSet.getInt(1) + ", " + resultSet.getString(2));
+            }
+            //preparedStatement.close();
+
+            sql = "Update trans set transtype='p2b' where id = 1";
+            stmt.executeUpdate(sql);
+            //no commit now
+
+
+            Thread anotherThred = new Thread()
+            {
+                public void run()
+                {
+                    try
+                    {
+                        System.out.println("Started another thread that has access to main thread objects");
+                        System.out.println("AnotherThread: Current Active connections in Connections Pool=" + jdbcConnectionPool.getActiveConnections());
+
+                        Connection thConn = jdbcConnectionPool.getConnection();
+
+                        PreparedStatement thPrepStmt = thConn.prepareStatement("Select id, transtype from Trans");
+                        ResultSet thRS = thPrepStmt.executeQuery();
+                        while (thRS.next())
+                        {
+                            System.out.println(thRS.getInt(1) + ", " + thRS.getString(2));
+                        }
+                        thPrepStmt.close();
+
+                        System.out.println("AnotherThread: Result set has been read");
+
+                        String thSql = "Update trans set transtype='balanceInquiry' where id = 2";
+                        Statement thStmt = thConn.createStatement();
+                        thStmt.executeUpdate(thSql);
+
+                        System.out.println("AnotherThread: Second record updated");
+                    }
+                    catch (Exception ex)
+                    {
+                        System.out.println("Exception:" + ex.getMessage());
+                    }
+                }
+            };
+
+            anotherThred.start();
+
+
+            System.out.println("Main thread is sleeping for 5 seconds");
+            Thread.sleep(2);
+            System.out.println("Main thread finished sleeping, and will commit the update now");
+
+            conn.commit();
+
+            System.out.println("Reading data in main thread");
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next())
+            {
+                System.out.println(resultSet.getInt(1) + ", " + resultSet.getString(2));
+            }
+            preparedStatement.close();
 
             //Thread.sleep(20000);
 
